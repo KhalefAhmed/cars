24cf885d85794f5211143602a05fa1fa00700617
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-04-13 00:10:56 +0200

    change in eureka server

--- eureka/src/main/java/com/udacity/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/udacity/eureka/EurekaApplication.java
@@ -1,11 +1,9 @@
-package com.example.eureka;
+package com.udacity.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
