46823237ee86295c0dc80b06a5a197e961994fb4
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-04-14 00:28:43 +0200

    change pricing-service to a microservice

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -1,8 +1,12 @@
 package com.udacity.pricing.domain.price;
-
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+
 
 @Repository
-public class PriceRepository {
+public interface PriceRepository extends CrudRepository<Price, Long> {
+
+    Price findByVehicleId(@Param("vehicle_id") Long vehicleId);
 
 }
